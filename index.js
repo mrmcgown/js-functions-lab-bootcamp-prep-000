b9373bbd8ccec80c8b636db7fa1872724900779e
Michael R. McGown <mrmcgown@gmail.com>
2018-03-12 16:01:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
+<<<<<<< HEAD
   return `Happy holidays, ${name}!`;
 } 
 
@@ -14,3 +15,7 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
+=======
+  return 'Happy holidays, ' + '${name}!';
+} 
+>>>>>>> c28a6874d19e9454fe742091ebc55d6e4096e9d3
